154d1a998d23cb3b9d48ebfcc7ec66dfe8b3befa
Quinn <austinq23@student.jhs.net>
2022-02-11 14:15:00 -0800

    I tried to make it less lines

--- src/Frogsimulator.java
+++ src/Frogsimulator.java
@@ -1,22 +1,15 @@
 import java.util.Random;
-
 public class Frogsimulator{
     private int goalDistance;
     private int maxHops;
-
 public Frogsimulator(int goalDistance, int maxHops) {
         this.goalDistance = goalDistance;
-        this.maxHops = maxHops;
-    }
-
+        this.maxHops = maxHops;}
     private int hopDistance(){
         Random ran = new Random();
-
         // generating integer
         int randint = ran.nextInt(30-10) + 10;
-        return randint;
-    }
-
+        return randint;}
 public boolean simulate(){
     int totaldistance = 0;
     for(int i = 0; i < maxHops; i++){
@@ -24,17 +17,10 @@ public boolean simulate(){
         if( totaldistance < 0){return false;}
         if(totaldistance >= goalDistance){
             return true;}
-    }
-    return false;
-}
-
+    }return false;}
 public double runSimulations(int num){
     double totaltrue = 0;
     for(int i = 0; i < num; i++){
         if( simulate() == true){
-            totaltrue++;
-        }
-    } 
-    return totaltrue / num; //simulation over total
-}
-}
+            totaltrue++;}} 
+    return totaltrue / num;}}
