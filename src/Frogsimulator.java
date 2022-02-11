de9441275aaa4aeb6f04bae8d1e1d951d8400b79
Quinn <austinq23@student.jhs.net>
2022-02-11 14:17:59 -0800

    LESS LINES

--- src/Frogsimulator.java
+++ src/Frogsimulator.java
@@ -1,26 +1,6 @@
 import java.util.Random;
-public class Frogsimulator{
-    private int goalDistance;
-    private int maxHops;
-public Frogsimulator(int goalDistance, int maxHops) {
-        this.goalDistance = goalDistance;
-        this.maxHops = maxHops;}
-    private int hopDistance(){
-        Random ran = new Random();
-        // generating integer
-        int randint = ran.nextInt(30-10) + 10;
-        return randint;}
-public boolean simulate(){
-    int totaldistance = 0;
-    for(int i = 0; i < maxHops; i++){
-        totaldistance += hopDistance();
-        if( totaldistance < 0){return false;}
-        if(totaldistance >= goalDistance){
-            return true;}
-    }return false;}
-public double runSimulations(int num){
-    double totaltrue = 0;
-    for(int i = 0; i < num; i++){
-        if( simulate() == true){
-            totaltrue++;}} 
-    return totaltrue / num;}}
+public class Frogsimulator{private int goalDistance;private int maxHops;
+public Frogsimulator(int goalDistance, int maxHops) {this.goalDistance = goalDistance;this.maxHops = maxHops;}
+private int hopDistance(){Random ran = new Random();int randint = ran.nextInt(30-10) + 10;return randint;}
+public boolean simulate(){int totaldistance = 0;for(int i = 0; i < maxHops; i++){totaldistance += hopDistance();if( totaldistance < 0){return false;}if(totaldistance >= goalDistance){return true;} }return false;}
+public double runSimulations(int num){double totaltrue = 0;for(int i = 0; i < num; i++){if( simulate() == true){totaltrue++;}} return totaltrue / num;}}
